64e614628199f147de15940b0ab1ef03eaa425c8
Ningappa <ningappahirekudi889@gmail.com>
2026-08-06 10:55:27 +0530

    Fix Login import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Activities from "./pages/Activities";
